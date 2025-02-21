ca9430d40192506ea40d7ee490938885ae7a1ecc
Rashmi Suralkar <prashmi.89@gmail.com>
2025-02-21 10:36:32 +0000

    static pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "https://github.com/prashmi9/Rashmi-Portfolio",
+  basePath: "/prashmi9/Rashmi-Portfolio",
   images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
